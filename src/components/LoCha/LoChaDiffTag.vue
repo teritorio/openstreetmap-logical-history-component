e44d79f2f1911a2fbc64194006b81e48937e66ab
Noé Viricel <noe.viricel@wazo-lab.io>
2025-09-10 18:34:23 +0200

    chore: remove badge

--- src/components/LoCha/LoChaDiffTag.vue
+++ src/components/LoCha/LoChaDiffTag.vue
@@ -24,65 +24,23 @@ defineProps<{
         'no_changes': action[1] !== 'reject',
       }"
     >
-      <span
-        v-if="(action && action[2] && Object.keys(action[2]).length)
-          || undefined" class="badge"
-      >
-        {{ (action && action[2] && Object.keys(action[2]).length)
-          || undefined }}
-      </span>
       {{ type }}: {{ action[0] }}
-      <template v-if="action && action[2]">
-        ⮟
-      </template>
-      <!-- <template v-if="action && action[2]" #dropdown>
-        <el-dropdown-menu v-for="(option, i) in action[2]" :key="i">
-          <el-dropdown-item class="clearfix">
-            {{ i }}
-            <template v-if="Array.isArray(option)">
-              <ul>
-                <li v-for="op in option" :key="op">
-                  {{ op }}
-                </li>
-              </ul>
-            </template>
-            <template v-else>
-              {{ option }}
-            </template>
-          </el-dropdown-item>
-        </el-dropdown-menu>
-      </template> -->
     </div>
   </template>
 </template>
 
 <style lang="css" scoped>
-.tag,
-.badge {
+.tag {
   display: inline-flex;
   justify-content: center;
   align-items: center;
   border: 1px solid;
   padding: 0 6px;
   border-color: v-bind('loChaColors.delete');
-}
-
-.tag {
   position: relative;
   border-radius: 10px;
   font-size: 0.75rem;
   white-space: nowrap;
-}
-
-.badge {
-  position: absolute;
-  top: 0;
-  right: calc(1px + 18px / 2);
-  border-radius: 10px;
-  transform: translateY(-50%) translateX(100%);
-  height: 18px;
-  width: fit-content;
-  color: #ffffff;
 }
 
 .no_changes {
