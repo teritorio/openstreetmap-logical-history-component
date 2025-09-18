fc6b38a8310c40a15cb52f5d729651774152b79a
Noé Viricel <noe.viricel@wazo-lab.io>
2025-09-18 15:07:45 +0200

    feat: add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import * as Sentry from '@sentry/vue'
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from './router'
 import '@/main.css'
 
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: 'https://fc5e84020ff3efe66cc8d61008908f04@sentry.teritorio.xyz/21',
+  integrations: [],
+})
 
 app.use(router)
 app.mount('#app')
