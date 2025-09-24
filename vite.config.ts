b3065987d952bdfbb327eda98b380b54409dca61
Noé Viricel <noe.viricel@wazo-lab.io>
2025-09-24 15:57:48 +0200

    chore: setup sentry sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,38 @@
 import type { UserConfig } from 'vite'
 import { resolve } from 'node:path'
+import process from 'node:process'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import dts from 'vite-plugin-dts'
 
-const baseConfig = {
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  plugins: [vue()],
-} satisfies UserConfig
-
 export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  const baseConfig = {
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+    plugins: [
+      vue(),
+      sentryVitePlugin({
+        authToken: env.VITE_SENTRY_AUTH_TOKEN,
+        org: 'teritorio',
+        project: 'openstreetmap-logical-history-dev-frontend',
+        url: 'https://sentry.teritorio.xyz',
+      }),
+    ],
+  } satisfies UserConfig
+
   // Demo build config
   if (command === 'build' && mode === 'demo') {
     return {
       base: '/openstreetmap-logical-history-component/',
       build: {
         outDir: './demo-dist',
+        sourcemap: true,
       },
       ...baseConfig,
     }
@@ -59,6 +72,9 @@ export default defineConfig(({ command, mode }) => {
   // Local Development config
   return {
     ...baseConfig,
+    build: {
+      sourcemap: true,
+    },
     server: {
       proxy: {
         '/api': {
