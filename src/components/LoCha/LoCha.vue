c018b9f4e25ce7c2d88a34bc562057b65bae093c
Noé Viricel <noe.viricel@wazo-lab.io>
2026-05-13 16:38:37 +0700

    style: remove padding on locha when single group

--- src/components/LoCha/LoCha.vue
+++ src/components/LoCha/LoCha.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { GroupSlotProps, LoChaData, ObjectDetailSlotProps } from '@/types'
-import { provide, watch } from 'vue'
+import { computed, provide, watch } from 'vue'
 import LoChaGroupList from '@/components/LoCha/LoChaGroupList.vue'
 import { useLoCha } from '@/composables/useLoCha'
 import { LOCHA_INSTANCE_ID_KEY, LOCHA_KEY, MAP_STYLE_URL_KEY, REASON_COLLAPSED_KEY } from '@/constants/injectionKeys'
@@ -33,8 +33,11 @@ provide(LOCHA_KEY, loChaInstance)
 
 const {
   featureCount,
+  groups,
   setLoCha,
 } = loChaInstance
+
+const isSingle = computed(() => groups.value.length === 1)
 
 watch(() => props.data, (newValue) => {
   if (newValue) {
@@ -44,7 +47,7 @@ watch(() => props.data, (newValue) => {
 </script>
 
 <template>
-  <section class="locha">
+  <section class="locha" :class="{ 'locha--single': isSingle }">
     <p v-if="!featureCount" class="user-feedback">
       ⚠️ No data
     </p>
@@ -71,6 +74,10 @@ watch(() => props.data, (newValue) => {
   flex-direction: column;
   height: inherit;
   padding: 1rem;
+}
+
+.locha--single {
+  padding: 0;
 }
 
 .locha-group-list {
